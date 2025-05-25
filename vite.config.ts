d60c0856066bd293f8ae4af8ebdfac245a5259df
shoerl <hoerl.s@northeastern.edu>
2025-05-24 23:28:53 -0400

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+
+// Determine repository name, defaulting for local builds
+const repoName = process.env.GITHUB_REPOSITORY
+  ? process.env.GITHUB_REPOSITORY.split('/')[1]
+  : 'shelfify'; // Default repository name
+
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_REF_NAME === 'main' ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/',
+  base: process.env.GITHUB_REF_NAME === 'main' ? `/${repoName}/` : '/',
 });
